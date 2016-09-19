a046cab96512b7e655a6ca3898ba02e6e3a8217f
jlegendre <jeremy.legendre@free.fr>
2016-09-19 20:53:03 +0200

    ajout GetReady et TapTap et taille entre les platf

---
+++ Assets/Scripts/MouvementGetReady.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouvementGetReady : MonoBehaviour {
+
+    Vector2 vitesse = new Vector2(-1.5f, 0);
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = vitesse;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale == 1)
+        {
+            gameObject.SetActive(false);
+
+        }
+
+    }
+}
--- Assets/Scripts/MoveTextureBas.cs
+++ Assets/Scripts/MoveTextureBas.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MoveTextureBas : MonoBehaviour {
 
+
     Vector2 vitesse = new Vector2(-2.5f, 0);
     // Use this for initialization
     void Start()
